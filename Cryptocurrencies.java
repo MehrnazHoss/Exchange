055af3a45842c2b7f76dd53a6a734eb496b33ce0
Mehrnaz <153914759+MehrnazHoss@users.noreply.github.com>
2024-06-05 01:28:08 +0330

    create homepage

---
+++ Cryptocurrencies.java
@@ -0,0 +1,9 @@
+package com.example.final_project;
+
+public class Cryptocurrencies {
+    public String nameOfCryptocurrency;
+    public long rateOfCryptocurrency;
+    public float percentageOfChanges;
+    public long maxRateOfCryptocurrency;
+    public long minRateOfCryptocurrency;
+}
